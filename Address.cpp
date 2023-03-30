4c0fe6249b966d8ad07e80cffb159ff812f7997c
Emine Yılmaz <91937411+emineyilmazz@users.noreply.github.com>
2023-03-30 17:53:53 +0300

    Add files via upload

---
+++ Address.cpp
@@ -0,0 +1,18 @@
+
+/*
+    * Address.cpp
+    * Metotların içinin doldurulduğu, constructor metodunun olduğu yerdir.
+    * Üçüncü Proje
+    * 06.01.2023
+    * emine yilmaz  emine.yilmaz@stu.fsm.edu.tr
+*/
+
+
+
+#include "Address.h"
+
+Address::Address(string city, string country){
+	this->City = city;
+	this->Country = country;
+}
+
